049680d0a699b15ba60d7fb5ba0a4e915587b3f6
GZ2829 <gzucchero24@gmail.com>
2018-08-05 22:29:54 -0600

    contact

--- server/app.js
+++ server/app.js
@@ -15,9 +15,48 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Static Folder
-app.use('../client', express.static(path.join(__dirname, 'client')))
+app.use('../client/src/app/components/contact/contact.component.html', express.static(path.join(__dirname, 'contact')))
 
 
-app.listen(4540, () =>
-    console.log('Up and Running on 4040')
+app.listen(4040, () =>
+    console.log('Up and Running on 4200')
 );
+
+app.post('/send', (req,res)=>{
+    const output= `
+    <h1>You have a new message</h1>
+    <h3>Contact Details</h3>
+    <ul>
+    <li>Name: ${req.body.contactName}</li>
+    <li>Email:${req.body.contactEmail}</li>
+    </ul>
+    <h4>Message:</h4>
+    <p>${req.body.contactMessage}</p>`
+
+    nodemailer.createTestAccount((err, account) => {
+        // create reusable transporter object using the default SMTP transport
+       
+    
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Node Mail Contact" <gregdev28@outlook.com>', 
+            to: 'Greg Zucchero, gzucchero24@gmail.com', 
+            subject: 'New message from portfolio site', 
+            text: '', 
+            html: output 
+        };
+    
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    
+            res.render('contact', {Msg: 'Email Has Been Sent'})
+            
+        });
+    });
+    
+})
+
