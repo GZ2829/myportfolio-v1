f5175b593facefc91ef2289d8ee306140e380d2a
GZ2829 <gzucchero24@gmail.com>
2018-09-13 13:44:41 -0600

    restructure

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Static Folder
-app.use(express.static(__dirname, './client/dist/myportfolio')
+app.use(express.static(__dirname, './dist/myportfolio')
 )
 
 // app.use(function (req, res, next) {
@@ -41,7 +41,7 @@ app.use(express.static(__dirname, './client/dist/myportfolio')
 // // });
 
 app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/myportfolio/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/myportfolio/index.html'));
 });
 
 
