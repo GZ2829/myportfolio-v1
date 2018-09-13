33ecdc087c5ba06a8ee7e4b4c0e1dd43717c0ec6
GZ2829 <gzucchero24@gmail.com>
2018-09-13 12:07:05 -0600

    first commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // Static Folder
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname, '/dist'))
 // app.use(function (req, res, next) {
 
 //     // Website you wish to allow to connect
