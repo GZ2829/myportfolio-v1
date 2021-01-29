3d813d44552280b2f8dbca9ff94f30d66c7bf9dd
Greg Zucchero <lavandor9@pop-os.localdomain>
2021-01-28 22:18:25 -0600

    font selection and video stretch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { NgModule, Component } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router'
 
 import { AboutComponent } from './components/about/about.component';
-import { HomeComponent } from './components/home/home.component';
 import { CryptoportfolioComponent } from './components/cryptoportfolio/cryptoportfolio.component';
 import { TransportconsolidatedComponent } from './components/transportconsolidated/transportconsolidated.component';
 import { BusinesswebsiteComponent } from './components/businesswebsite/businesswebsite.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'crypto-portfolio', component: CryptoportfolioComponent },
   { path: 'transport-consolidated', component: TransportconsolidatedComponent },
@@ -23,4 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent,AboutComponent,CryptoportfolioComponent,TransportconsolidatedComponent,BusinesswebsiteComponent,ContactComponent]
+export const routingComponents = [AboutComponent,CryptoportfolioComponent,TransportconsolidatedComponent,BusinesswebsiteComponent,ContactComponent]
